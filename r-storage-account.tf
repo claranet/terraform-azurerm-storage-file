0293855a9251179a0fa3bf6c42206941f095368c
Laurent Piroelle <laurent.piroelle@fr.clara.net>
2022-10-14 17:48:44 +0200

    AZ-869: Fix queue properties

--- r-storage-account.tf
+++ r-storage-account.tf
@@ -35,6 +35,8 @@ module "storage_account" {
   storage_blob_data_protection = null
   storage_blob_cors_rule       = null
 
+  queue_properties_logging = null
+
   advanced_threat_protection_enabled = var.advanced_threat_protection_enabled
   https_traffic_only_enabled         = !contains([for s in var.file_shares : s.enabled_protocol], "NFS") && var.https_traffic_only_enabled
   min_tls_version                    = var.min_tls_version
