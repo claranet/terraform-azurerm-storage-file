079c1f44418d3cc3f19cee8e234342dfd07d38e5
Laurent Piroelle <laurent.piroelle@fr.clara.net>
2022-10-21 10:51:57 +0200

    AZ-869: Add `_netdev` to mount options

--- locals.tf
+++ locals.tf
@@ -14,8 +14,8 @@ locals {
 EOF
   mount_options = {
     for s in var.file_shares : s.name => s.enabled_protocol == "NFS" ?
-    "rw,noatime,vers=4.1,rsize=1048576,wsize=1048576,namlen=255,acregmin=600,acregmax=600,acdirmin=600,acdirmax=600" :
-    "nofail,credentials=${local.cifs_creds_file_path},dir_mode=0777,file_mode=0777,serverino,nosharesock,actimeo=30"
+    "rw,noatime,_netdev,vers=4.1,rsize=1048576,wsize=1048576,namlen=255,acregmin=600,acregmax=600,acdirmin=600,acdirmax=600" :
+    "nofail,_netdev,credentials=${local.cifs_creds_file_path},dir_mode=0777,file_mode=0777,serverino,nosharesock,actimeo=30"
   }
 
   mount_endpoints = { for s in var.file_shares :
